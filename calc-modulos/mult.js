f26b717ecf6a21d1b29c21b6b79ec0a893a3c0a0
git config --global user.name Anyel <anyel.martich@gmail.com>
2021-05-26 21:17:37 +0200

    Corrigiendo error en comentario

--- calc-modulos/mult.js
+++ calc-modulos/mult.js
@@ -1,7 +1,7 @@
 //Función de la multiplicacion que recibe dos números y retorna la mult de dichos números
 
 function mult(a, b) {
-    return a - b;
+    return a * b;
   }
   //Exporta el módulo mult
   module.exports = mult;
