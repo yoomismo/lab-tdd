ca3decbd3b76ba155d1deaac384c560073843659
Yo <a@a.e>
2021-05-26 21:33:41 +0200

    sin conflictos

--- pruebas-modulos/raiz.test.js
+++ pruebas-modulos/raiz.test.js
@@ -4,10 +4,7 @@ const raiz = require('../calc-modulos/raiz');
 test('la raiz de 121 es igual a 11.0', () => {
   expect(raiz(121)).toBe(11.0);
 });
-<<<<<<< HEAD
-test('la raiz de 8 es igual a 2.828', () => { 
-=======
+
 test('la raiz de 8 es igual a 2.828', () => {
->>>>>>> raizCuadrada
   expect(raiz(8)).toBe(2.82842712474619);
 });
