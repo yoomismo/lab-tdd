f200ceb5e3aa1eeb6900db509f21bcbd1f9d7809
git config --global user.name Anyel <anyel.martich@gmail.com>
2021-05-26 20:48:56 +0200

    Funcion con la multiplicacion y comentario actualizado

---
+++ calc-modulos/mult.js
@@ -0,0 +1,7 @@
+//Función de la multiplicacion que recibe dos números y retorna la mult de dichos números
+
+function mult(a, b) {
+    return a - b;
+  }
+  //Exporta el módulo resta
+  module.exports = mult;
--- pruebas-modulos/mult.test.js
+++ pruebas-modulos/mult.test.js
@@ -1,6 +1,6 @@
+// Archivo que contiene la implementacion de nuestra prueba actual:
+const mult = require('../calc-modulos/mult');
 
-
-//Codigo para el valor esperado de la prueba
 test('multiplicar 7 * 8', () => {
   expect(mult(7, 8)).toBe(56);
 });
